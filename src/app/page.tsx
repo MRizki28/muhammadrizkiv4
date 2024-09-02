121022b9ace0d646e2afbce99db7ab1e6e8d467d
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-09-02 20:35:06 +0800

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 
+import Profile from "@/components/home/Profile";
 import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
     <main className="max-w-screen-xl self-stretch m-auto w-full">
       <Navbar></Navbar>
+      <article className="px-6 sm:px-12 py-6">
+        <Profile></Profile>
+      </article>
     </main>
   );
 }
