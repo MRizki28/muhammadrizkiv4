0f00c5a89e4d64a32aa535e68cc32ac02e0daa2e
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-09-03 23:59:18 +0800

    update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,26 +1,58 @@
-import Link from "next/link";
-import { MdDarkMode } from "react-icons/md";
+'use client';
+
+import { useTheme } from 'next-themes';
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
+import { MdDarkMode, MdSunny } from 'react-icons/md';
 
 export default function Navbar() {
+    const [mounted, setMounted] = useState(false);
+    const { setTheme, resolvedTheme } = useTheme();
+
+    useEffect(() => {
+        setMounted(true);
+    }, []);
+
+    const handleThemeToggle = () => {
+        setTheme(resolvedTheme === 'dark' ? 'light' : 'dark');
+    };
+
     return (
         <header className="flex flex-row items-center px-6 sm:px-12 py-6 font-sans">
             <div className="mr-6 mb-[2px]">
-                <Link href="/" aria-current="page" className="transition text-black duration-200 ease-in-out flex flex-row items-center hover:text-red-700 font-extrabold text-sm no-underline tracking-widest uppercase">MRizki</Link>
+                <Link href="/" aria-current="page" className="transition text-black duration-200 ease-in-out flex flex-row items-center hover:text-red-700 font-extrabold text-sm no-underline tracking-widest uppercase dark:text-white dark:hover:text-red-700">
+                    MRizki
+                </Link>
             </div>
             <nav className="flex flex-grow">
                 <ul className="flex items-center m-0">
                     <li className="ml-5 mb-0">
-                        <Link href="/" aria-current="page" className="transition duration-200 ease-in-out flex flex-row items-center text-black hover:text-red-600 uppercase text-xs font-bold no-underline">About</Link>
+                        <Link href="/" aria-current="page" className="transition duration-200 ease-in-out flex flex-row items-center text-black hover:text-red-600 uppercase text-xs font-bold no-underline dark:text-white dark:hover:text-red-700">
+                            About
+                        </Link>
                     </li>
                     <li className="ml-5 mb-0">
-                        <Link href="/" aria-current="page" className="transition duration-200 ease-in-out flex flex-row items-center text-black hover:text-red-600 uppercase text-xs font-bold no-underline">Project</Link>
+                        <Link href="/" aria-current="page" className="transition duration-200 ease-in-out flex flex-row items-center text-black hover:text-red-600 uppercase text-xs font-bold no-underline dark:text-white dark:hover:text-red-700">
+                            Project
+                        </Link>
                     </li>
                 </ul>
                 <div className="ml-auto">
-                    <MdDarkMode className="text-gray-700 text-3xl cursor-pointer hover:bg-black hover:rounded-full  hover:text-white" />
+                    {mounted && (
+                        resolvedTheme === 'dark' ? (
+                            <MdDarkMode
+                                onClick={handleThemeToggle}
+                                className="text-3xl cursor-pointer text-white"
+                            />
+                        ) : (
+                            <MdSunny
+                                onClick={handleThemeToggle}
+                                className="text-gray-700 text-3xl cursor-pointer"
+                            />
+                        )
+                    )}
                 </div>
-
             </nav>
         </header>
-    )
+    );
 }
