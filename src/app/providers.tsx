98d5c58b649bdb171c4add7935dc85c03680d134
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-09-07 20:24:56 +0800

    update

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,7 +1,5 @@
-'use client'
-
 import { ThemeProvider } from 'next-themes'
 
 export function Providers({ children }: { children: React.ReactNode }) {
-    return <ThemeProvider attribute="class" defaultTheme='system' enableSystem>{children}</ThemeProvider>
+    return <ThemeProvider attribute="class" defaultTheme='ligth' enableSystem>{children}</ThemeProvider>
 }
